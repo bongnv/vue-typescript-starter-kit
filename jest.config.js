d3fe3171c0774854a24969a0fbeae48a68801973
Bong Nguyen <van.bong@grabtaxi.com>
2020-04-06 22:22:01 +0800

    feat: add vuetify

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  setupFiles: ["<rootDir>/tests/unit/setup.ts"],
+  testMatch: ["**/(*.)test.ts"],
+  collectCoverage: true,
+  moduleNameMapper: {
+    "^vuetify/lib$": "vuetify"
+  },
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
 };
